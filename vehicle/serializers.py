f40fa450d372dc2615fc979714e6f88f1b541398
Vladimir Semikov <voesem@ya.ru>
2023-11-02 19:37:07 +0300

    Создана модель "Пробег"

--- vehicle/serializers.py
+++ vehicle/serializers.py
@@ -1,15 +1,24 @@
 from rest_framework import serializers
 
-from vehicle.models import Car, Moto
+from vehicle.models import Car, Moto, Milage
 
 
 class CarSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Car
         fields = '__all__'
 
 
 class MotoSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Moto
+        fields = '__all__'
+
+
+class MilageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Milage
         fields = '__all__'
