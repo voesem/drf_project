7fbe318eb612e9b73a5c9810055e0c9789fa12ef
Vladimir Semikov <voesem@ya.ru>
2023-11-02 19:36:14 +0300

    Создана модель "Пробег"

--- vehicle/models.py
+++ vehicle/models.py
@@ -23,3 +23,18 @@ class Moto(models.Model):
     class Meta:
         verbose_name = 'мотоцикл'
         verbose_name_plural = 'мотоциклы'
+
+
+class Milage(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, null=True, blank=True)
+    moto = models.ForeignKey(Moto, on_delete=models.CASCADE, null=True, blank=True)
+    milage = models.PositiveIntegerField(verbose_name='пробег')
+    year = models.PositiveSmallIntegerField(verbose_name='год регистрации')
+
+    def __str__(self):
+        return f'{self.moto if self.moto else self.car} - {self.year}'
+
+    class Meta:
+        verbose_name = 'пробег'
+        verbose_name_plural = 'пробег'
+        ordering = ('-year',)
