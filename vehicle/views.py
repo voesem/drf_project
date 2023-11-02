498e3c10316188c6ae29f16b0efe32c3d5e87877
Vladimir Semikov <voesem@ya.ru>
2023-11-02 19:37:32 +0300

    Описан ViewSet для работы с моделью "Пробег"

--- vehicle/views.py
+++ vehicle/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, generics
 
 from vehicle.models import Car, Moto
-from vehicle.serializers import CarSerializer, MotoSerializer
+from vehicle.serializers import CarSerializer, MotoSerializer, MilageSerializer
 
 
 class CarViewSet(viewsets.ModelViewSet):
@@ -30,3 +30,7 @@ class MotoUpdateAPIView(generics.UpdateAPIView):
 
 class MotoDestroyAPIView(generics.DestroyAPIView):
     queryset = Moto.objects.all()
+
+
+class MilageCreateAPIView(generics.CreateAPIView):
+    serializer_class = MilageSerializer
