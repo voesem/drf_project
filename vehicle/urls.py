cc9e6022c573458380e82b981019d1edfa98e687
Vladimir Semikov <voesem@ya.ru>
2023-11-02 20:09:39 +0300

    Создан новый эндпоинт для вывода пробегов мотоциклов (урлы и контроллеры).

--- vehicle/urls.py
+++ vehicle/urls.py
@@ -4,7 +4,7 @@ from vehicle.apps import VehicleConfig
 from rest_framework.routers import DefaultRouter
 
 from vehicle.views import CarViewSet, MotoCreateAPIView, MotoListAPIView, MotoRetrieveAPIView, MotoUpdateAPIView, \
-    MotoDestroyAPIView, MilageCreateAPIView
+    MotoDestroyAPIView, MilageCreateAPIView, MotoMilageListAPIView
 
 app_name = VehicleConfig.name
 
@@ -19,4 +19,5 @@ urlpatterns = [
     path('moto/delete/<int:pk>/', MotoDestroyAPIView.as_view(), name='moto-delete'),
 
     path('milage/create/', MilageCreateAPIView.as_view(), name='milage-create'),
+    path('moto/milage/', MotoMilageListAPIView.as_view(), name='moto-milage'),
 ] + router.urls
--- vehicle/views.py
+++ vehicle/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, generics
 
-from vehicle.models import Car, Moto
-from vehicle.serializers import CarSerializer, MotoSerializer, MilageSerializer
+from vehicle.models import Car, Moto, Milage
+from vehicle.serializers import CarSerializer, MotoSerializer, MilageSerializer, MotoMilageSerializer
 
 
 class CarViewSet(viewsets.ModelViewSet):
@@ -34,3 +34,8 @@ class MotoDestroyAPIView(generics.DestroyAPIView):
 
 class MilageCreateAPIView(generics.CreateAPIView):
     serializer_class = MilageSerializer
+
+
+class MotoMilageListAPIView(generics.ListAPIView):
+    queryset = Milage.objects.filter(moto__isnull=False)
+    serializer_class = MotoMilageSerializer
