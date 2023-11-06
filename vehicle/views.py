8b85e7627748a53cae20560dc2aeba05ed34c7ee
Vladimir Semikov <voesem@ya.ru>
2023-11-06 21:02:04 +0300

    Описаны права доступа для контроллера CarViewSet

--- vehicle/views.py
+++ vehicle/views.py
@@ -1,6 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
 from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated
 
 from vehicle.models import Car, Moto, Milage
 from vehicle.serializers import CarSerializer, MotoSerializer, MilageSerializer, MotoMilageSerializer, \
@@ -10,6 +11,7 @@ from vehicle.serializers import CarSerializer, MotoSerializer, MilageSerializer,
 class CarViewSet(viewsets.ModelViewSet):
     serializer_class = CarSerializer
     queryset = Car.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class MotoCreateAPIView(generics.CreateAPIView):
