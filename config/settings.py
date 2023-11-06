fbad47304fa76656eb9a2d0f0eb485c32f6a146f
Vladimir Semikov <voesem@ya.ru>
2023-11-06 21:01:27 +0300

    Внесены настройки авторизации для проекта в целом

--- config/settings.py
+++ config/settings.py
@@ -133,5 +133,9 @@ AUTH_USER_MODEL = 'users.User'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        # 'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
+    ]
 }
