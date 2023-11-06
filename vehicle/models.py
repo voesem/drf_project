17a64a07d10b5161b365c2924c70947be524719d
Vladimir Semikov <voesem@ya.ru>
2023-11-06 21:25:27 +0300

    добавлено поле владельца для моделей Car и Moto

--- vehicle/models.py
+++ vehicle/models.py
@@ -1,9 +1,11 @@
+from django.conf import settings
 from django.db import models
 
 
 class Car(models.Model):
     title = models.CharField(max_length=150, verbose_name='название')
     description = models.TextField(verbose_name='описание')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return f'{self.title}'
@@ -16,6 +18,7 @@ class Car(models.Model):
 class Moto(models.Model):
     title = models.CharField(max_length=150, verbose_name='название')
     description = models.TextField(verbose_name='описание')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return f'{self.title}'
