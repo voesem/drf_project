00869504e150f942759371b8b4e941f0ec28e00a
Vladimir Semikov <voesem@ya.ru>
2023-11-12 16:46:01 +0300

    Описн класс валидатора

---
+++ vehicle/validators.py
@@ -0,0 +1,16 @@
+import re
+
+from rest_framework.serializers import ValidationError
+
+
+class TitleValidator:
+
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, value):
+        reg = re.compile('^[a-zA-Z0-9\.\-\ ]+$')
+        tmp_val = dict(value).get(self.field)
+
+        if not bool(reg.match(tmp_val)):
+            raise ValidationError('title is not ok')
