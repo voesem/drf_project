269768921179b798ac296ccb640733c0eba4a293
voesem <voesem@gmail.com>
2023-12-20 13:40:35 +0300

    Внесены изменения в настройки проекта на использование пакета dotenv и адреса брокера redis

--- config/settings.py
+++ config/settings.py
@@ -9,17 +9,21 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f4ph1bn(rdwpj1f3uz92fq^imww14=qi=(1n*=gntz#iz^8sum'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -84,9 +88,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'drf_project',
-        'USER': 'postgres',
-        'PASSWORD': '123'
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST')
     }
 }
 
@@ -155,8 +160,8 @@ CSRF_TRUSTED_ORIGINS = [
     "https://read-and-write.example.com",
 ]
 
-CURRENCY_API_URL = 'https://api.currencyapi.com/'
-CURRENCY_API_KEY = 'cur_live_I5tww4ATLmLlOkL8sjKmcYUPDqcRmsz0Qd5nzoN8'
+CURRENCY_API_URL = os.getenv('CURRENCY_API_URL')
+CURRENCY_API_KEY = os.getenv('CURRENCY_API_KEY')
 
-CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
-CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
